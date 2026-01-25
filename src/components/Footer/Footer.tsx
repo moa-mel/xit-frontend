66819b7e0c5e40046070cf30a7acd8ab88309472
Olaitan <olaitanmaryakano@gmail.com>
2026-01-25 20:48:58 +0100

    remove previous footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,37 +12,7 @@ const Footer = () => {
         <footer>
             <div className="footerSection">
                 <div className="footerContainer">
-                    <div className="footerUpSection">
-                        <div className="footerHeader">
-                            <Image
-                                src={logo}
-                                alt="Logo"
-                                width={15}
-                                height={15}
-                            />
-                            <span className="footerText">Xit</span>
-                        </div>
-                        <div className="footerSocial">
-                            <Image
-                                src={linkedIn}
-                                alt="linkedIn"
-                                width={15}
-                                height={15}
-                            />
-                            <Image
-                                src={facebook}
-                                alt="facebook"
-                                width={15}
-                                height={15}
-                            />
-                            <Image
-                                src={x}
-                                alt="x"
-                                width={15}
-                                height={15}
-                            />
-                        </div>
-                    </div>
+                    
                     <div className="footerLine"></div>
                     <p className="footerDownSection">© 2025 Xit. All Rights Reserved.</p>
                 </div>
