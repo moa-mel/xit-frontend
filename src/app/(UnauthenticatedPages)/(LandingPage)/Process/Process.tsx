c1eec4b745ee9f809fb69c409e896472bfa8258b
Olaitan <olaitanmaryakano@gmail.com>
2026-01-25 20:47:26 +0100

    remove the previous design

--- src/app/(UnauthenticatedPages)/(LandingPage)/Process/Process.tsx
+++ src/app/(UnauthenticatedPages)/(LandingPage)/Process/Process.tsx
@@ -1,60 +1,13 @@
 "use client";
 import "./Process.css"
 import Image from "next/image";
-import Plus from "@/assets/images/Plus icon.png"
-import Minus from "@/assets/images/Plus icon(1).png"
 import { useState } from "react";
 
 const Process = () => {
-    const [expand, setExpand] = useState<number | null>(null)
-
-    const toggleExpand = (i: number) =>{
-        setExpand(expand === i ? null : i)
-    }
+    
     return (
         <div className="processSection">
-            <div className={`processContainer ${expand === 0 ? 'expanded' : ''}`}>
-                <div className="processWrap1">
-                    <div className="processWrapLeft">
-                        <h2 className="processWrapLeftText1">01</h2>
-                        <h3 className="processWrapLeftText2">Live Streaming videos</h3>
-                    </div>
-                    <div className="processWrapRight" onClick={() => toggleExpand(0)}>
-                        <Image
-                            src={expand === 0 ? Minus : Plus}
-                            alt={expand === 0 ? "Minus" : "Plus"}
-                            width={30}
-                            height={30}
-                        />
-                    </div>
-                </div>
-                {expand === 0 && (
-                    <div className="processContent">
-                        <p>Live streaming videos is a feature that allows you to broadcast your content in real-time to your audience. This feature is available on our platform and can be used to create engaging and interactive content for your audience.</p>
-                    </div>
-                )}
-            </div>
-            <div className={`processContainer2 ${expand === 1 ? 'expanded' : ''}`}>
-                <div className="processWrap2">
-                    <div className="processWrapLeft">
-                        <h2 className="processWrapLeftText1">02</h2>
-                        <h3 className="processWrapLeftText2">Podcast</h3>
-                    </div>
-                    <div className="processWrapRight" onClick={() => toggleExpand(1)}>
-                        <Image
-                            src={expand === 1 ? Minus : Plus}
-                            alt={expand === 1 ? "Minus" : "Plus"}
-                            width={30}
-                            height={30}
-                        />
-                    </div>
-                </div>
-                {expand === 1 && (
-                    <div className="processContent">
-                        <p>Podcast is a feature that allows you to create and share audio content with your audience. This feature is available on our platform and can be used to create engaging and interactive content for your audience.</p>
-                    </div>
-                )}
-            </div>
+            
         </div>
     )
 }
--- src/app/(UnauthenticatedPages)/(LandingPage)/Services/Services.tsx
+++ src/app/(UnauthenticatedPages)/(LandingPage)/Services/Services.tsx
@@ -1,68 +1,11 @@
 "use client";
 import "./Services.css"
 import Image from "next/image";
-import illustration1 from "@/assets/images/Illustration(2).png"
-import illustration2 from "@/assets/images/Illustration(3).png"
-import ArrowUp from "@/assets/images/Icon(1).png"
 
 const Services = () => {
     return (
         <div className="serviceSection">
-            <div className="serviceText">
-                <h3>Services</h3>
-                <p>At our digital marketing agency, we offer a range of services <br /> to help businesses grow and succeed online. These services include:</p>
-            </div>
-            <div className="serviceContainer">
-                <div className="serviceCard1">
-                    <div className="serviceCard1Content">
-                        <div className="servviceCard1Left">
-                            <h3 className="serviceCard1Text">Live Streaming</h3>
-                            <div className="serviceCard1TextButton">
-                                <Image
-                                    src={ArrowUp}
-                                    alt="arrowUp"
-                                    width={20}
-                                    height={20}
-                                />
-                                <p>Learn more</p>
-                            </div>
-                        </div>
-                        <div className="serviceCard1Image">
-                            <Image
-                                src={illustration1}
-                                alt="illustration1"
-                                width={150}
-                                height={150}
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className="serviceCard2">
-                    <div className="serviceCard2Content">
-                        <div className="servviceCard2Left">
-                            <h3 className="serviceCard2Text">Podcast</h3>
-                            <div className="serviceCard2TextButton">
-                                <Image
-                                    src={ArrowUp}
-                                    alt="arrowUp"
-                                    width={20}
-                                    height={20}
-                                />
-                                <p>Learn more</p>
-                            </div>
-                        </div>
-
-                        <div className="serviceCard2Image">
-                            <Image
-                                src={illustration2}
-                                alt="illustration2"
-                                width={150}
-                                height={150}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div >
 
     )
