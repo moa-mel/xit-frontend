1414490c03d2aa7442387d1cea81055fa2b19d30
Olaitan <olaitanmaryakano@gmail.com>
2026-01-26 08:55:31 +0100

    update the features and benefit section

--- src/app/(UnauthenticatedPages)/(LandingPage)/Benefits/Benefits.tsx
+++ src/app/(UnauthenticatedPages)/(LandingPage)/Benefits/Benefits.tsx
@@ -1,11 +1,71 @@
 "use client";
 import "./Benefits.css"
 import Image from "next/image";
+import group from "@/assets/images/Group 33632.png"
+import Icon from "@/assets/images/Icon.png"
+import Icon2 from "@/assets/images/Icon(1).png"
+import Icon3 from "@/assets/images/Icon(2).png"
+
 
 const Benefits = () => {
     return (
         <div className="benefitSection">
-            
+            <div className="benefitContainer">
+                <div className="benefitHeader">
+                    <h3>WHAT YOU CAN DO</h3>
+                </div>
+                <div className="benefitMain">
+                    <div>
+                        <h1>Create On-Demand <br/>
+                        Podcasts <span> Effortlessly</span></h1>
+                        <div className="benefitItems">
+                            <div className="benefitList">
+                                <Image
+                                    src={Icon}
+                                    alt="Icon"
+                                    width={20}
+                                    height={20}
+                                    priority={true}
+                                />
+                                <p>Easy Episode Uploads</p>
+                                <span>Quickly upload your audio files and get <br/>
+                                 them live.</span>
+                            </div>
+                            <div className="benefitList">
+                                <Image
+                                    src={Icon2}
+                                    alt="Icon2"
+                                    width={20}
+                                    height={20}
+                                    priority={true}
+                                />
+                                <p>Organize Into Series</p>
+                                <span>Group episodes into seasons or themes <br/> for easy navigation.</span>
+                            </div>
+                            <div className="benefitList">
+                                <Image
+                                    src={Icon3}
+                                    alt="Icon3"
+                                    width={20}
+                                    height={20}
+                                    priority={true}
+                                />
+                                <p>Background Listening</p>
+                                <span>Let your audience listen while multitasking <br/>
+                                 on their devices.</span>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div>
+                        <Image
+                            src={group}
+                            alt="group"
+                            className="groupImage"
+                            priority={true} />
+                    </div>
+                </div>
+            </div>
         </div >
 
     )
--- src/app/(UnauthenticatedPages)/(LandingPage)/Features/Features.tsx
+++ src/app/(UnauthenticatedPages)/(LandingPage)/Features/Features.tsx
@@ -2,12 +2,66 @@
 import "./Features.css"
 import Image from "next/image";
 import { useState } from "react";
+import arrow from "@/assets/images/arrow right.png"
+import video from "@/assets/images/Group 33566.png"
+import podcast from "@/assets/images/Group 33567.png"
 
 const Features = () => {
-    
+
     return (
         <div className="featureSection">
-            
+            <div className="featureContainer">
+                <div className="featureHeader">
+                    <h3>One single platform</h3>
+                    <h2>Two Ways To Connect</h2>
+                </div>
+                <div className="featureWrap">
+                    <div className="featureLeft">
+                        <div className="mainFeature">
+                            <div className="featureItem">
+                                <Image
+                                    src={video}
+                                    alt="video"
+                                    width={20}
+                                    height={20}
+                                    priority={true}
+                                />
+                                <p>Live Streaming</p>
+                                <span>Host conversations as they happen.<br />
+                                    Join discussions, listen in, and engage with <br />
+                                    others — all in the moment.</span>
+                            </div>
+                            <div className="featureItem">
+                                <Image
+                                    src={podcast}
+                                    alt="podcast"
+                                    width={20}
+                                    height={20}
+                                    priority={true}
+                                />
+                                <p>Podcasts</p>
+                                <span>Publish structured, on-demand episodes <br />
+                                    your audience can listen to anytime.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="featureRight">
+                        <h3>Choose how you want to share.</h3>
+                        <p>Go live, publish podcasts, or do both without setting up multiple tools or workflows.</p>
+                        <button>
+                            Get Started
+                            <Image
+                                src={arrow}
+                                alt="arrow icon"
+                                width={20}
+                                height={20}
+                                priority={true}
+                            />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
